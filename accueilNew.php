b47f95088b0b4a3ab168c88c01b7dfd31b0e97db
LouisTrouilleux <58275850+LouisTrouilleux@users.noreply.github.com>
2020-04-19 21:48:20 +0200

    Add files via upload

---
+++ accueilNew.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>ECE e-bay</title>
+	<meta charset="utf-8"/>
+	<link href="general.css" rel="stylesheet" type="text/css" />
+	
+</head>
+
+<div id="content">
+    <body>
+        <div id="topbar">
+            <?php include 'header.php'; ?>
+       
+            <?php include 'nav.php'; ?>
+        </div>
+            <div id="content1">
+	        <p>Avec notre site d'enchères en ligne, vous pouvez mettre en vente ou achetez de nombreux objets ! <br>
+	    Vous pouvez achetez immédiatement un produit, enchérir, ou négocier directement avec le vendeur. <br>
+	    Nous vous souhaitons un agréable séjour sur notre site ! </p>
+
+	    <ul>
+		    <li>Achat immédiat : Vous achetez directement un objet au prix fixé par le vendeur.</li>
+		    <li>Enchères : Vous proposez un prix plus élevé que le dernier enchereur. Si personne d'autre ne renchérit, vous obtenez l'objet. </li>
+		    <li>Négociation : Vous pouvez discutez et proposez jusqu'à 5 fois avec le vendeur pour décider du prix qui vous conviendra.</li>
+        </ul>
+    
+
+	    <p class="contact">ECE e-bay <br>
+		    7 quai de Grenelle<br>
+		    75015 Paris<br> <br>
+		    (+33) 01 02 03 04 05 06
+        </p>
+        </div>
+
+        <?php include 'footer.php'; ?>
+
+    </body>
+</div>
+</html>
